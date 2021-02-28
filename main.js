119a3f1edcb1bb939ecf203d1f301ed97876a72d
Spooky-png <MikeHinds4@gmail.com>
2021-02-27 22:51:58 -0500

    I hope I didn't dun goof

---
+++ main.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '+';
+
+client.once('ready', () => {
+    console.log('Spookbot is online!')
+});
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('Swamp Sux');
+    } else if (command == 'github'){
+        message.channel.send('https://www.github.com/Spooky-png');
+    }
+});
+
+
+
+
+
+
+
+client.login('token bruh');
